6f0fbaf47756e192b080b5e576992040711e7a0d
Victor Nwagu <nwaguvictor>
2022-11-05 00:10:50 +0100

    feat: initial setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http')
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send(`If you're seeing this page, then server is up...`)
+})
+
+const server = http.createServer(app);
+
+const port = 5000;
+
+server.listen(port, () => console.log(`Server started on port: ${port}`))
+
